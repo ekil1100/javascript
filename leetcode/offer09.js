1dabeae25d83287d2175dce05bc51a3f8583ec04
Ekil <keithleaves@gmail.com>
2022-02-21 00:17:45 +0800

    .

---
+++ leetcode/offer09.js
@@ -0,0 +1,40 @@
+// leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+
+https: var CQueue = function () {
+  this.stack1 = []
+  this.stack2 = []
+}
+
+/**
+ * @param {number} value
+ * @return {void}
+ */
+CQueue.prototype.appendTail = function (value) {
+  this.stack1.push(value)
+}
+
+/**
+ * @return {number}
+ */
+CQueue.prototype.deleteHead = function () {
+  if (this.stack1.length == 0 && this.stack2.length == 0) {
+    return -1
+  }
+
+  if (this.stack2.length > 0) {
+    return this.stack2.pop()
+  }
+
+  while (this.stack1.length > 0) {
+    console.log(x)
+    this.stack2.push(this.stack1.pop())
+    console.log(this.stack2)
+  }
+
+  return this.stack2.pop()
+}
+
+var obj = new CQueue()
+obj.appendTail(5)
+obj.appendTail(2)
+var v = obj.deleteHead()
