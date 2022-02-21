00269a11aee13fbbccd8e387c4ba2eddf4ea5a4e
Ekil <keithleaves@gmail.com>
2022-02-21 09:53:30 +0800

    .

---
+++ leetcode/offer30.js
@@ -0,0 +1,58 @@
+/**
+ * initialize your data structure here.
+ */
+var MinStack = function () {
+  this._min = []
+  this.stack = []
+  this._top = undefined
+}
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function (x) {
+  if (this._top === undefined) {
+    this._min.push(x)
+  } else {
+    if (x < this._min[this._min.length - 1]) {
+      this._min.push(x)
+    } else {
+      this._min.push(this._min[this._min.length - 1])
+    }
+  }
+  this._top = x
+  this.stack.push(x)
+}
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+  this._min.pop()
+  this._top = this.stack[this.stack.length - 2]
+  return this.stack.pop()
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+  return this._top
+}
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.min = function () {
+  return this._min[this._min.length - 1]
+}
+
+var obj = new MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-1)
+obj.min()
+obj.top()
+obj.pop()
+obj.min()
